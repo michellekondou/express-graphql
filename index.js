f1879531908df9f5ce68254a0e5ddff7ef3f8385
Michelle Kondou <michellekondou@gmail.com>
2018-09-07 17:58:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,32 +6,46 @@ import { createServer } from 'http'
 import app from './server'
 
 //ssl credentials
-const privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
-const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
+// const privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
+// const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+// const credentials = { key: privateKey, cert: certificate };
 
-const httpServer = http.createServer(app)
-const httpsServer = spdy
-    .createServer(credentials, app)
-    .listen(3001, (error) => {
-        if (error) {
-            console.error(error)
-            return process.exit(1)
-        } else {
-            console.log('Listening on port:3001 ')
-        }
-    })
+// const httpServer = http.createServer(app)
+// const httpsServer = spdy
+//     .createServer(credentials, app)
+//     .listen(3005, (error) => {
+//         if (error) {
+//             console.error(error)
+//             return process.exit(1)
+//         } else {
+//             console.log('Listening on port:3005 ')
+//         }
+//     })
 
+// let currentApp = app
+
+// httpServer.listen(3006, () => {
+//     console.log('Server listening on port 3006')
+// })
+
+// if (module.hot) {
+//     module.hot.accept(['./server'], () => {
+//         httpsServer.removeListener('request', currentApp)
+//         httpsServer.on('request', app)
+//         currentApp = app
+//     })
+// }
+const server = http.createServer(app)
 let currentApp = app
 
-httpServer.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Server listening on port 3000')
 })
 
 if (module.hot) {
     module.hot.accept(['./server'], () => {
-        httpsServer.removeListener('request', currentApp)
-        httpsServer.on('request', app)
+        server.removeListener('request', currentApp)
+        server.on('request', app)
         currentApp = app
     })
 }
